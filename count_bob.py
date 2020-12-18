9d74a10547285a8a5bac9a71fd9289f4a903ea8b
keeeva <70092439+keeeva@users.noreply.github.com>
2020-12-18 21:38:07 +0100

    Update count_bob.py
    added the function alternative

--- count_bob.py
+++ count_bob.py
@@ -1,3 +1,5 @@
+# a little program that counts how many times bob is in a string
+
 s = 'azcbobobegghakl'
 
 count = 0
@@ -6,8 +8,14 @@ for letter in range(0, len(s)):
         count += 1
 print(count)
 
+# same thing just as function count_bob()
 
-
-
-
+def count_bob(st):
+    count = 0
+    for letter in range(0, len(st)):
+        if st[letter: letter + 3] == 'bob':
+            count += 1
+    return count
+    
+print(count_bob(s))
 
