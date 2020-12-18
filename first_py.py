6e14f45697295bad95389c27499ea7ee049c15d0
keeeva <70092439+keeeva@users.noreply.github.com>
2020-12-18 21:29:59 +0100

    Update first_py.py
    adding the function without using list comprehension

--- first_py.py
+++ first_py.py
@@ -1,4 +1,4 @@
-# just a quick function that squares the elements of a list
+# just a quick function that squares the elements of a list using list comprehension
 
 a_random_list = [1, 3, 6, 8]
 
@@ -7,3 +7,13 @@ def sq(l):
   return l
   
 print(sq(a_random_list)) # [1, 9, 36, 64]
+
+# the same idea without using list comprehension 
+
+def sq2(l):
+  l2 = []
+  for i in l:
+    l2.append(i**2)
+  return l2
+
+print(sq2(a_random_list)) # [1, 9, 36, 64]
